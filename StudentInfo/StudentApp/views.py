2da4ce51fdaffc48e94deaf0eca97af55386b356
vinilspoorthik@gmail.com <svspoorthik@gmail.com>
2025-12-06 11:37:52 +0530

    This is our first commit

---
+++ StudentInfo/StudentApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Hello, welcome to the Student Info App!")
+def student(request):
+    # Logic to fetch and display student details
+    return render(request, 'student.html')
+
+# Create your views here.
---
+++ StudentInfo/StudentInfo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for StudentInfo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudentInfo.settings')
+
+application = get_wsgi_application()
